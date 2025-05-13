2a3b5924daf48b2eb216b70f6617a069466d7f7c
kumar.krish07022002@gmail.com <kumar.krish07022003@gmail.com>
2025-05-14 01:44:58 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// document.querySelectorAll('.nav-link').forEach(item => {
+//     item.addEventListener('click', function(e) {
+//         e.preventDefault(); // Prevent default behavior of anchor links
+        
+//         // Get the target section ID from the data-section attribute
+//         const sectionToShow = this.getAttribute('data-section');
+        
+//         // Hide all sections first
+//         document.querySelectorAll('.content-section').forEach(section => {
+//             section.style.display = 'none';
+//         });
+        
+//         // Show the clicked section
+//         document.getElementById(sectionToShow).style.display = 'block';
+//     });
+// });
